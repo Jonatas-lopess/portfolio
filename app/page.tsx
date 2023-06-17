703d7debb0df1453727e6ba75726126b76af3ba9
Jonatas Lopes <jonatinhalopes30@gmail.com>
2023-06-17 01:31:28 -0300

    feat(home): title section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
+  const ICON_SIZE = 48
+
   return (
     <main>
-      <h1>Home</h1>
+      <div>
+        <h3>Full Stack Developer</h3>
+        <h1>Jonatas Lopes</h1>
+        <div>
+          <Image alt="linkedin" src="/linkedin.svg" width={ICON_SIZE} height={ICON_SIZE} />
+          <Image alt="github" src="/github.svg" width={ICON_SIZE} height={ICON_SIZE} />
+          <Image alt="curriculum" src="/download.svg" width={ICON_SIZE} height={ICON_SIZE} />
+        </div>
+      </div>
     </main>
   )
 }
